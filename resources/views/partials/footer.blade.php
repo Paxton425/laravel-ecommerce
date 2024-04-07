14b61bb589ab8b71027a70b9c239ce09e5fafe4a
unknown <phillgaxsphe@gmail.com>
2024-04-08 01:11:03 +0200

    first commit

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="footer-content container">
-        <div class="made-with">Made by Abdelrahman</div>
+        <div class="made-with">Made by Sphelele</div>
 
         {{-- Footer menu --}}
         {{ menu('footer', 'partials.menus.footer') }}
